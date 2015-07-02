9a731075ab137ba65980e0fc342b132338ddfd7b
KevinOrtiz <kevinandresortizmerchan@gmail.com>
2015-07-02 08:24:31 -0500

    Falta boton cancelar por que voy a ala u

--- js/validacionPerfil.js
+++ js/validacionPerfil.js
@@ -7,9 +7,6 @@
  * 
  */
 
-
-
-
 function habilitarcajasusuario(){
 	document.getElementById("labelNombre").style.background ='white';
 	document.getElementById("labelApellido").style.background ='white';
@@ -46,160 +43,7 @@ function habilitarcajasauto(){
 	
 }
 
-function validarnombre(event){
-  if(event.which == 13){
-  	var texto = document.getElementById("labelNombre").value;
-  	if (texto.search(/^[a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑüÜ_\s]+$/) != -1){
 
-		document.getElementById("labelApellido").focus();
-  	}
-	
-	else{
-
-		document.getElementById("labelNombre").style.backgroundColor="red";
-		document.getElementById("labelNombre").style.color = 'white';
-
-	}
-
-	}
-	else{
-		document.getElementById("labelNombre").style.backgroundColor="white";
-		document.getElementById("labelNombre").style.color = 'black';
-
-	}
-
-  }
-	
-function validarapellido(event){
-	if(event.which == 13){
-  	var texto = document.getElementById("labelApellido").value;
-  	if (texto.search(/^[a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑüÜ_\s]+$/) != -1){
-		document.getElementById("labelCelular").focus();
-  	}
-	
-	else{
-
-		document.getElementById("labelApellido").style.backgroundColor="red";
-		document.getElementById("labelApellido").style.color = 'white';
-
-	}
-
-	}
-	else{
-
-		document.getElementById("labelApellido").style.backgroundColor="white";
-		document.getElementById("labelApellido").style.color = 'black';
-
-	}
-
-
-}
-
-function validarcelular(event){
-	if(event.which == 13){
-  	var texto = document.getElementById("labelCelular").value;
-  	if (texto.search(/^[0-9]+$/) != -1){
-		document.getElementById("labelsexo").focus();
-  	}
-	
-	else{
-
-		document.getElementById("labelCelular").style.backgroundColor="red";
-		document.getElementById("labelCelular").style.color = 'white';
-
-	}
-
-	}
-	else{
-		document.getElementById("labelCelular").style.backgroundColor="white";
-		document.getElementById("labelCelular").style.color = 'black';
-
-	}
-
-}
-
-function validarsexo(event)
-{
-	if(event.which == 13){
-  	var texto = document.getElementById("labelsexo").value;
-  	if (texto.search(/^[a-u]+$/) != -1){
-		
-		document.getElementById("labelNombre").disabled=true;
-		document.getElementById("labelApellido").disabled=true;
-		document.getElementById("labelCelular").disabled=true;
-		document.getElementById("labelsexo").disabled=true;
-		
-		
-
-  	}
-	
-	else{
-
-		document.getElementById("labelsexo").style.backgroundColor="red";
-		document.getElementById("labelsexo").style.color = 'white';
-
-		}
-	
-
-	}
-	else
-	{
-		document.getElementById("labelsexo").style.backgroundColor="white";
-		document.getElementById("labelsexo").style.color = 'black';
-	}
-}
-
-function validarplaca(event)
-{
- if(event.which == 13){
- 	var texto = document.getElementById("labelplaca").value;
- 		if(texto.search(/^([a-z]+[0-9]+)|([0-9]+[a-z]+)/i)!= -1)
- 		{
- 			document.getElementById("labelcapacidad").focus();
- 		}
- 		else
- 		{
- 		document.getElementById("labelplaca").style.backgroundColor="red";
-		document.getElementById("labelplaca").style.color = 'white';
-
-
- 		}
- }
-
- else{
- 	document.getElementById("labelplaca").style.backgroundColor="white";
-	document.getElementById("labelplaca").style.color = 'black';
- }
-}
-
-function validarcapacidad(event){
-
- if(event.which == 13){
-  	var texto = document.getElementById("labelcapacidad").value;
-  	if (texto.search(/^[0-6]+$/) != -1){
-		document.getElementById("labelcapacidad").disabled=true;
-		document.getElementById("labelplaca").disabled =true;
-  	}
-	
-	else{
-
-		document.getElementById("labelcapacidad").style.backgroundColor="red";
-		document.getElementById("labelcapacidad").style.color = 'white';
-
-	}
-
-}
-
-}
-
-function habilitarcajas(){
-	var listacajas = document.getElementsByTagName("input");
-
-		for(var i=0 ; i < 4;i++){
-			listacajas[i].disabled = false;
-		}
-		document.getElementsByTagName("input")[0].focus();
-}
 
 function cerrardatos(){
 	var listacajas = document.getElementsByTagName("input");
@@ -234,13 +78,4 @@ var listacajas = document.getElementsByTagName("input");
 }
 
 
-function habilitarcarro(){
-	var listacajas = document.getElementsByTagName("input");
 
-		for(var i=4 ; i < 6;i++){
-			listacajas[i].disabled = false;
-		}
-		document.getElementsByTagName("input")[4].focus();
-
-
-}
