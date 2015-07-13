521c1ece0cb4c0b6c9938eb1ce23f65540fa5a14
rodrigo <rodfcast@espol.edu.ec>
2015-07-13 12:02:03 -0500

    Se agrego la navegacion entre las diferentes paginas de la aplicacion

---
+++ js/barra.js
@@ -0,0 +1,12 @@
+function cargarPerfil(){
+  window.location.href="perfil.html"; 
+}
+
+function cargarNoticias(){
+  window.location.href="noticias.html"; 
+}
+
+function cargarMensajes(){
+  window.location.href="chat.html"; 
+}
+
