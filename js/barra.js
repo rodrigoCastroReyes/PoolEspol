12a312485ae91e73710e0a913c048e8e14898647
rodrigo <rodfcast@espol.edu.ec>
2015-07-14 10:29:02 -0500

    Se agrego rutas al perfil, Opcion para salir de la aplicacion

--- js/barra.js
+++ js/barra.js
@@ -10,3 +10,23 @@ function cargarMensajes(){
   window.location.href="chat.html"; 
 }
 
+function salir(){
+	menu_salir.style.display="flex";
+	contenedor_rutas.style.opacity=0.5;
+}
+
+function aceptarCierre(){
+	window.location.href="cerrar_sesion.html"; 
+}
+
+function cancelarCierre(){
+	contenedor_rutas.style.opacity=1;
+	menu_salir.style.display="none";
+}
+
+function init(){
+	aceptar_cierre.addEventListener('click',aceptarCierre,false);
+	cancelar_cierre.addEventListener('click',cancelarCierre,false);
+}
+
+window.addEventListener('load',init,false);
