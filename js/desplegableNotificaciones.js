07be6c200ad1b24e5f62ec8f855d096da744e662
jorge Vergara <jorenver@espol.edu.ec>
2015-07-15 01:14:12 -0500

    desplegable Notificaciones

---
+++ js/desplegableNotificaciones.js
@@ -0,0 +1,31 @@
+function cargarNotificaciones(){
+  window.location.href="notificaciones.html"; 
+}
+
+$(document).ready(function () {
+	$("#notificaciones").click(function () {
+		$("#contenedor_Notificacion").show();
+		
+	    $("#sign_box").show();
+	    return false;
+	});
+
+	$("body").click(function () {
+		$("#contenedor_Notificacion").hide();
+	    $("#sign_box").hide();
+	    return false;
+	});
+
+	$("").click(function () {
+		$("#contenedor_Notificacion").hide();
+	    $("#sign_box").hide();
+	    return false;
+	});
+
+	$("#mas").click(function () {
+		cargarNotificaciones();
+	    return false;
+	});
+
+
+});
