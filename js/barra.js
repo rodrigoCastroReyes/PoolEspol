3cc3660fc178671c46f5ecdf1728fd976d9607f3
rodrigo <rodfcast@espol.edu.ec>
2015-07-15 00:18:25 -0500

    Se agrego efecto en la opcion de cerrar sesion

--- js/barra.js
+++ js/barra.js
@@ -15,8 +15,31 @@ function cargarNotificaciones(){
 }
 
 function salir(){
+	
 	menu_salir.style.display="flex";
-	//contenedor_rutas.style.opacity=0.5;
+
+	var rutas=document.getElementById("contenedor_rutas");
+	var perfil=document.getElementById("contenedor_perfil")
+	var chat=document.getElementById("contenido");
+
+	if(rutas!=null){
+		rutas.style.opacity=0.5;//pagina de noticias
+		var botones=document.querySelectorAll("button");
+		for(var i=0;i<botones.length;i++){
+			botones[i].disabled=true;
+		}
+	}
+	if(perfil!=null){
+		perfil.style.opacity=0.5;//pagina de perfil
+		var botones=document.querySelectorAll("button");
+		for(var i=0;i<botones.length;i++){
+			botones[i].disabled=true;
+		}
+	}
+
+	if(chat!=null){
+		chat.style.opacity=0.5;
+	}
 }
 
 function aceptarCierre(){
@@ -24,8 +47,31 @@ function aceptarCierre(){
 }
 
 function cancelarCierre(){
-	//contenedor_rutas.style.opacity=1;
 	menu_salir.style.display="none";
+
+	var rutas=document.getElementById("contenedor_rutas");
+	var perfil=document.getElementById("contenedor_perfil")
+	var chat=document.getElementById("contenido");
+
+	if(rutas!=null){
+		rutas.style.opacity=1;//pagina de noticias
+		var botones=document.querySelectorAll("button");
+		for(var i=0;i<botones.length;i++){
+			botones[i].disabled=false;
+		}
+	}
+	if(perfil!=null){
+		perfil.style.opacity=1;//pagina de perfil
+		var botones=document.querySelectorAll("button");
+		for(var i=0;i<botones.length;i++){
+			botones[i].disabled=false;
+		}
+	}
+
+	if(chat!=null){
+		chat.style.opacity=1;
+	}
+
 }
 
 function init(){
