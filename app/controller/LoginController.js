ea99fea8e44ffb664975b48493f67bdd71f4e426
LuisBSC14 <luallain@hotmail.com>
2015-08-31 02:46:36 -0500

    Update LoginController.js
    Agregadas las funciones para autenticar y registrar

--- app/controller/LoginController.js
+++ app/controller/LoginController.js
@@ -45,3 +45,48 @@ exports.login=function(request,response){
 	   });
 	   
 };
+
+
+
+exports.autenticar=function(request,response){
+	   var soap = require('soap');
+	   var url = 'http://ws.espol.edu.ec/saac/wsandroid.asmx?WSDL';
+	   var args = {authUser: request.body.usuario, authContrasenia: request.body.contraseña};
+	   var resp;
+	   soap.createClient(url, function(err, client) {
+	   	client.autenticacion(args, function(err, result){
+	   		resp = result.autenticacionResult;
+	   		console.log(resp);
+			if (resp){
+				db.encontrarUsuario(request.body.usuario).then(function (user){
+				if(!user){
+					console.log("Autenticacion correcta");
+				}else{			
+					console.log('Ud ya se encuentra registrado');}
+					response.sendfile(html_dir + 'index.html');
+				}).catch(function(err){
+					console.log(err);
+					response.sendfile(html_dir + 'index.html');
+				});
+			}
+			else{ 
+				console.log('Ud no es usuario Espol');
+				response.sendfile(html_dir + 'index.html');
+			}
+	   	});
+	   });
+};
+
+exports.registrar= function(request,response){
+		if(request.body.carro=='si'){
+			var datos = {nick: 'request.body', password: 'asasasasasasa', nombre: request.body.nombre, apellidos: request.body.apellido, sexo: request.body.sexo, telefono: request.body.telefono, id_carro: request.body.placa, foto: 'asasasasasasa'};
+			db.guardarUsuario(datos);
+			var datos_carro = {
+placa: request.body.placa, foto: 'asasasasasasa', capacidad: request.body.capacidad};
+			db.guardarCarro(datos_carro);
+		}else{
+			var datos = {nick: 'request.body', password: 'asasasasasasa', nombre: request.body.nombre, apellidos: request.body.apellido, sexo: request.body.sexo, telefono: request.body.telefono, id_carro: null, foto: 'asasasasasasa'};
+			db.guardarUsuario(datos);
+		}
+		console.log('Registrado Correctamente');
+}
