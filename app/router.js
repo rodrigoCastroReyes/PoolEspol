2b17e203ae4dd81449350f8e8665430e2e244e9e
LuisBSC14 <luallain@hotmail.com>
2015-08-31 02:45:28 -0500

    Update router.js

--- app/router.js
+++ app/router.js
@@ -10,6 +10,8 @@ var router=express.Router();
 
 router.get('/',LoginController.index);
 router.post('/login',LoginController.login);
+router.post('/autenticar',LoginController.autenticar);
+router.post('/registrar',LoginController.registrar);
 
 router.get('/noticias',NoticiasController.noticias);
 router.get('/Rutas',NoticiasController.obtenerRutasNoticias);
